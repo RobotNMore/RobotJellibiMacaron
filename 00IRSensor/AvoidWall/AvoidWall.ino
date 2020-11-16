77be07a925c64627b8a573a047009b30095bd776
RobotNMore <57121256+RobotNMore@users.noreply.github.com>
2020-11-16 14:49:49 +0900

    Update AvoidWall.ino

--- 00IRSensor/AvoidWall/AvoidWall.ino
+++ 00IRSensor/AvoidWall/AvoidWall.ino
@@ -56,7 +56,7 @@ void setup() {
   pinMode(10, OUTPUT); // LED
   pinMode(11, OUTPUT);
   Serial.begin(9600);
-  //오른쪽 버튼 누르면 
+  //오른쪽 버튼 누르면 동작
   while (!(digitalRead(4) == 0))
   {
     _loop();
